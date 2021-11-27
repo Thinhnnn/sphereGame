18d2a65b114d642be1d1489cc7305c10e0d78df5
thinhtm99 <thinhtm99@gmail.com>
2021-11-27 15:07:17 +0700

    + First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; } // static singleton
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this.gameObject);
+        else
+            Instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
